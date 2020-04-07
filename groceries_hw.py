6396245c7e4dd6d8f4c83e8c9d46d8a51e2e6879
David Foster <dlfosterii@yahoo.com>
2020-04-06 22:20:22 -0400

    upto menu item 2

--- groceries_hw.py
+++ groceries_hw.py
@@ -51,14 +51,74 @@ main_menu = '''
 
 '''
 
+
+
 while True:
     menu_choice = int(input(main_menu))
 
     # Add if/else statements for each menu item
+    if menu_choice == 1:
+        for i in indexes:
+            item = groceries[i]
+            print(f'{i}: {item}')
+    elif menu_choice == 2:
+        # Prompt for a new item until the just hit Enter
+        while True:
+            item = input('What do you need from the store? ')
 
+            if item == '':  # Alternatively: check if len(item) == 0
+                break
+            groceries.append(item)
+
+
+print(main_menu)
     # For each of these, add in code to handle adding/editing/removing items
 
     if menu_choice == 5:
         break
+
+# Start with an empty grocery list
+groceries = ['cow', 'chicken', 'mustard', 'eggs', 'bread', 'cheese']
+
+
+
+# After we break, python will move to the next unindented line of code after the loop
+
+# Print the grocery list with indexes
+indexes = range(len(groceries))
+for i in indexes:
+    item = groceries[i]
+    print(f'{i}: {item}')
+
+# Give them the chance to replace 
+start_index_to_replace = int(input('What start index to replace? '))
+end_index_to_replace = int(input('What end index to replace? '))
+
+if start_index_to_replace == end_index_to_replace:
+    # Prompt the user for the new item
+    new_item = input('What is the new item? ')
+
+    # - replace the item at that index with the new item
+    groceries[start_index_to_replace] = new_item
+else:
+    # gather replacements
+    replacements = []
+    while True:
+        new_item = input('What is the new item? ')
+        if new_item == '':
+            break
+        replacements.append(new_item)
+        print(replacements)
+    print('-------about to do weird thing--------')
+    print(groceries)
+    groceries[start_index_to_replace:end_index_to_replace] = replacements
+    print('-------just did weird thing--------')
+    print(groceries)
+
+
+# - print the updated combined list
+for i in indexes:
+    item = groceries[i]
+    print(f'{i}: {item}')
 
 print('Thank you for using the grocery list app!')
